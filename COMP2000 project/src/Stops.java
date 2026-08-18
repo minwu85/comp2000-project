04305ffa7fa114ec095818c5a92d2cd87b37f742
AshtonMagri449147226 <ashton.magri@students.mq.edu.au>
2026-08-18 10:05:59 +1000

    idk

--- COMP2000 project/src/Stops.java
+++ COMP2000 project/src/Stops.java
@@ -1,7 +1,13 @@
 public class Stops {
-    
+    int x;
+    int y;
+    String name;
+    int capacity;
 
-    Stops(int x, int y, String name){
-
+    Stops(int x, int y, String name, int capacity){
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.capacity = capacity;
     }
 }
