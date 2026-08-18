24ca20a09097afbedec65c35e2402939de0c76d9
dannyboi212 <daniel.harvey1@students.mq.edu.au>
2026-08-18 13:58:53 +1000

    Train class changes

--- COMP2000 project/src/Vehicles.java
+++ COMP2000 project/src/Vehicles.java
@@ -6,10 +6,11 @@ public class Vehicles {
     //Train train1 = new Train(200, "Choo Choo", line1);
 
     //train1.setBounds(width/2 -normalStation/2 + 100, height/2 -normalStation/2, normalStation, normalStation);
+    public Vehicles(){
+        
+    }
 
-
-
-    Vehicles(int capacity, String name, Routes route){
+    public Vehicles(int capacity, String name, Routes route){
         this.capacity = capacity;
         this.name = name;
         this.route = route;
@@ -17,3 +18,4 @@ public class Vehicles {
 
 
 }
+
